58c62bdf2be376a75779cb32a5d478008fdf0de7
minseo-o <halia0709@gmail.com>
2023-08-24 18:30:04 +0900

    환자 확인 페이지 찐 완성

--- src/Projects/Patient_Check.java
+++ src/Projects/Patient_Check.java
@@ -44,6 +44,12 @@ public class Patient_Check extends CommonFrame {
 		JLabel lb = new JLabel(getIcon("images/Doctor_Patient_Check.png", Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT));
 	      lb.setBounds(0, 0, Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 	      add(lb);
+	    
+	    //버튼 클릭하면 ... 
+	      submitbtn.addActionListener(e -> {
+		    	dispose();
+		        new ().setVisible(true);
+		       });
+		    
 	}
-	
 }
